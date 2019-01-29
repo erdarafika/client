ecad5024603ce8055641b7558b8c6f0c189aa495
SR8 Devops <anwar.runs@gmail.com>
2019-01-29 20:27:09 +0700

    Change port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.listen(8080, () => {
-  console.log('App listening on port 8080')
+app.listen(9187, () => {
+  console.log('App listening on port 9187')
 })
