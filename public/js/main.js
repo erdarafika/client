413b58a4b85d9ff0b575a0e08246df7bee474480
SR8 Devops <anwar.runs@gmail.com>
2019-01-31 14:33:51 +0700

    Filter error message

--- public/js/main.js
+++ public/js/main.js
@@ -81,31 +81,35 @@ const showreply = function showreply(id) {
             let div = document.createElement('div')
             div.className = 'tweetEntry'
             const msg = JSON.parse(data)
-            verify(msg.signed, msg.pubkey).then(result => {
-                div.innerHTML = `
-                    <div class="row">
-                        <div id="d-public.${msg.timestamp}~${msg.pubkey}" class="tweetEntry-content">
-                            <span class="tweetEntry-avatar"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="fingerprint" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-fingerprint fa-w-14 fa-9x"><defs class=""><clipPath id="clip-rZlolpo0ZmCK" class=""><path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z" class=""></path></clipPath><mask x="0" y="0" width="100%" height="100%" id="mask-7IfAyCrJnlDP" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" class=""><rect x="0" y="0" width="100%" height="100%" fill="white" class=""></rect><g transform="translate(224 256)" class=""><g transform="translate(0, 0)  scale(0.5625, 0.5625)  rotate(0 0 0)" class=""><path fill="black" d="M256.12 245.96c-13.25 0-24 10.74-24 24 1.14 72.25-8.14 141.9-27.7 211.55-2.73 9.72 2.15 30.49 23.12 30.49 10.48 0 20.11-6.92 23.09-17.52 13.53-47.91 31.04-125.41 29.48-224.52.01-13.25-10.73-24-23.99-24zm-.86-81.73C194 164.16 151.25 211.3 152.1 265.32c.75 47.94-3.75 95.91-13.37 142.55-2.69 12.98 5.67 25.69 18.64 28.36 13.05 2.67 25.67-5.66 28.36-18.64 10.34-50.09 15.17-101.58 14.37-153.02-.41-25.95 19.92-52.49 54.45-52.34 31.31.47 57.15 25.34 57.62 55.47.77 48.05-2.81 96.33-10.61 143.55-2.17 13.06 6.69 25.42 19.76 27.58 19.97 3.33 26.81-15.1 27.58-19.77 8.28-50.03 12.06-101.21 11.27-152.11-.88-55.8-47.94-101.88-104.91-102.72zm-110.69-19.78c-10.3-8.34-25.37-6.8-33.76 3.48-25.62 31.5-39.39 71.28-38.75 112 .59 37.58-2.47 75.27-9.11 112.05-2.34 13.05 6.31 25.53 19.36 27.89 20.11 3.5 27.07-14.81 27.89-19.36 7.19-39.84 10.5-80.66 9.86-121.33-.47-29.88 9.2-57.88 28-80.97 8.35-10.28 6.79-25.39-3.49-33.76zm109.47-62.33c-15.41-.41-30.87 1.44-45.78 4.97-12.89 3.06-20.87 15.98-17.83 28.89 3.06 12.89 16 20.83 28.89 17.83 11.05-2.61 22.47-3.77 34-3.69 75.43 1.13 137.73 61.5 138.88 134.58.59 37.88-1.28 76.11-5.58 113.63-1.5 13.17 7.95 25.08 21.11 26.58 16.72 1.95 25.51-11.88 26.58-21.11a929.06 929.06 0 0 0 5.89-119.85c-1.56-98.75-85.07-180.33-186.16-181.83zm252.07 121.45c-2.86-12.92-15.51-21.2-28.61-18.27-12.94 2.86-21.12 15.66-18.26 28.61 4.71 21.41 4.91 37.41 4.7 61.6-.11 13.27 10.55 24.09 23.8 24.2h.2c13.17 0 23.89-10.61 24-23.8.18-22.18.4-44.11-5.83-72.34zm-40.12-90.72C417.29 43.46 337.6 1.29 252.81.02 183.02-.82 118.47 24.91 70.46 72.94 24.09 119.37-.9 181.04.14 246.65l-.12 21.47c-.39 13.25 10.03 24.31 23.28 24.69.23.02.48.02.72.02 12.92 0 23.59-10.3 23.97-23.3l.16-23.64c-.83-52.5 19.16-101.86 56.28-139 38.76-38.8 91.34-59.67 147.68-58.86 69.45 1.03 134.73 35.56 174.62 92.39 7.61 10.86 22.56 13.45 33.42 5.86 10.84-7.62 13.46-22.59 5.84-33.43z" transform="translate(-256 -256)" class=""></path></g></g></mask></defs><rect fill="currentColor" clip-path="url(#clip-rZlolpo0ZmCK)" mask="url(#mask-7IfAyCrJnlDP)" x="0" y="0" width="100%" height="100%" class=""></rect></svg></span>    
-                            <span class="tweetEntry-username">
-                                <b>${smartTruncate(msg.pubkey, 20)}</b>
-                            </span>
-                            <span class="tweetEntry-timestamp"> - ${moment(msg.timestamp).format('L h:m:s a')}</span>
-                            <div class="tweetEntry-text-container">
-                                ${result.message}
+            if (msg.signed !== undefined && msg.pubkey !== undefined) {
+                verify(msg.signed, msg.pubkey).then(result => {
+                    if (result.message) {
+                        div.innerHTML = `
+                        <div class="row">
+                            <div id="d-public.${msg.timestamp}~${msg.pubkey}" class="tweetEntry-content">
+                                <span class="tweetEntry-avatar"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="fingerprint" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-fingerprint fa-w-14 fa-9x"><defs class=""><clipPath id="clip-rZlolpo0ZmCK" class=""><path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z" class=""></path></clipPath><mask x="0" y="0" width="100%" height="100%" id="mask-7IfAyCrJnlDP" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" class=""><rect x="0" y="0" width="100%" height="100%" fill="white" class=""></rect><g transform="translate(224 256)" class=""><g transform="translate(0, 0)  scale(0.5625, 0.5625)  rotate(0 0 0)" class=""><path fill="black" d="M256.12 245.96c-13.25 0-24 10.74-24 24 1.14 72.25-8.14 141.9-27.7 211.55-2.73 9.72 2.15 30.49 23.12 30.49 10.48 0 20.11-6.92 23.09-17.52 13.53-47.91 31.04-125.41 29.48-224.52.01-13.25-10.73-24-23.99-24zm-.86-81.73C194 164.16 151.25 211.3 152.1 265.32c.75 47.94-3.75 95.91-13.37 142.55-2.69 12.98 5.67 25.69 18.64 28.36 13.05 2.67 25.67-5.66 28.36-18.64 10.34-50.09 15.17-101.58 14.37-153.02-.41-25.95 19.92-52.49 54.45-52.34 31.31.47 57.15 25.34 57.62 55.47.77 48.05-2.81 96.33-10.61 143.55-2.17 13.06 6.69 25.42 19.76 27.58 19.97 3.33 26.81-15.1 27.58-19.77 8.28-50.03 12.06-101.21 11.27-152.11-.88-55.8-47.94-101.88-104.91-102.72zm-110.69-19.78c-10.3-8.34-25.37-6.8-33.76 3.48-25.62 31.5-39.39 71.28-38.75 112 .59 37.58-2.47 75.27-9.11 112.05-2.34 13.05 6.31 25.53 19.36 27.89 20.11 3.5 27.07-14.81 27.89-19.36 7.19-39.84 10.5-80.66 9.86-121.33-.47-29.88 9.2-57.88 28-80.97 8.35-10.28 6.79-25.39-3.49-33.76zm109.47-62.33c-15.41-.41-30.87 1.44-45.78 4.97-12.89 3.06-20.87 15.98-17.83 28.89 3.06 12.89 16 20.83 28.89 17.83 11.05-2.61 22.47-3.77 34-3.69 75.43 1.13 137.73 61.5 138.88 134.58.59 37.88-1.28 76.11-5.58 113.63-1.5 13.17 7.95 25.08 21.11 26.58 16.72 1.95 25.51-11.88 26.58-21.11a929.06 929.06 0 0 0 5.89-119.85c-1.56-98.75-85.07-180.33-186.16-181.83zm252.07 121.45c-2.86-12.92-15.51-21.2-28.61-18.27-12.94 2.86-21.12 15.66-18.26 28.61 4.71 21.41 4.91 37.41 4.7 61.6-.11 13.27 10.55 24.09 23.8 24.2h.2c13.17 0 23.89-10.61 24-23.8.18-22.18.4-44.11-5.83-72.34zm-40.12-90.72C417.29 43.46 337.6 1.29 252.81.02 183.02-.82 118.47 24.91 70.46 72.94 24.09 119.37-.9 181.04.14 246.65l-.12 21.47c-.39 13.25 10.03 24.31 23.28 24.69.23.02.48.02.72.02 12.92 0 23.59-10.3 23.97-23.3l.16-23.64c-.83-52.5 19.16-101.86 56.28-139 38.76-38.8 91.34-59.67 147.68-58.86 69.45 1.03 134.73 35.56 174.62 92.39 7.61 10.86 22.56 13.45 33.42 5.86 10.84-7.62 13.46-22.59 5.84-33.43z" transform="translate(-256 -256)" class=""></path></g></g></mask></defs><rect fill="currentColor" clip-path="url(#clip-rZlolpo0ZmCK)" mask="url(#mask-7IfAyCrJnlDP)" x="0" y="0" width="100%" height="100%" class=""></rect></svg></span>    
+                                <span class="tweetEntry-username">
+                                    <b>${smartTruncate(msg.pubkey, 20)}</b>
+                                </span>
+                                <span class="tweetEntry-timestamp"> - ${moment(msg.timestamp).format('L h:m:s a')}</span>
+                                <div class="tweetEntry-text-container">
+                                    ${result.message}
+                                </div>
                             </div>
+                            <form id="f.public.${msg.timestamp}~${msg.pubkey}">
+                                <div class="optionalMedia" style="display:none;">
+                                    <input id="public.${msg.timestamp}~${msg.pubkey}" type="text" value="superman">
+                                    <img class="optionalMedia-img" src="https://i.imgur.com/kOhhPAk.jpg">
+                                </div>
+                                <div class="tweetEntry-action-list" style="line-height:32px;color: #b1bbc3;">
+                                    
+                                </div>
+                            </form>
                         </div>
-                        <form id="f.public.${msg.timestamp}~${msg.pubkey}">
-                            <div class="optionalMedia" style="display:none;">
-                                <input id="public.${msg.timestamp}~${msg.pubkey}" type="text" value="superman">
-                                <img class="optionalMedia-img" src="https://i.imgur.com/kOhhPAk.jpg">
-                            </div>
-                            <div class="tweetEntry-action-list" style="line-height:32px;color: #b1bbc3;">
-                                
-                            </div>
-                        </form>
-                    </div>
-                    `
-            })
+                        `
+                    }
+                })
+            }
             // target.parentNode.insertBefore(div, target.nextSibling);
             target.appendChild(div)
         });
@@ -233,31 +237,35 @@ const sig = function signed() {
         let div = document.createElement('div')
         div.className = 'tweetEntry'
         const msg = JSON.parse(data)
-        verify(msg.signed, msg.pubkey).then(result => {
-            div.innerHTML = `
-                <div class="row">
-                    <div id="d-public.${msg.timestamp}~${msg.pubkey}" class="tweetEntry-content">
-                        <span class="tweetEntry-avatar"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="fingerprint" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-fingerprint fa-w-14 fa-9x"><defs class=""><clipPath id="clip-rZlolpo0ZmCK" class=""><path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z" class=""></path></clipPath><mask x="0" y="0" width="100%" height="100%" id="mask-7IfAyCrJnlDP" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" class=""><rect x="0" y="0" width="100%" height="100%" fill="white" class=""></rect><g transform="translate(224 256)" class=""><g transform="translate(0, 0)  scale(0.5625, 0.5625)  rotate(0 0 0)" class=""><path fill="black" d="M256.12 245.96c-13.25 0-24 10.74-24 24 1.14 72.25-8.14 141.9-27.7 211.55-2.73 9.72 2.15 30.49 23.12 30.49 10.48 0 20.11-6.92 23.09-17.52 13.53-47.91 31.04-125.41 29.48-224.52.01-13.25-10.73-24-23.99-24zm-.86-81.73C194 164.16 151.25 211.3 152.1 265.32c.75 47.94-3.75 95.91-13.37 142.55-2.69 12.98 5.67 25.69 18.64 28.36 13.05 2.67 25.67-5.66 28.36-18.64 10.34-50.09 15.17-101.58 14.37-153.02-.41-25.95 19.92-52.49 54.45-52.34 31.31.47 57.15 25.34 57.62 55.47.77 48.05-2.81 96.33-10.61 143.55-2.17 13.06 6.69 25.42 19.76 27.58 19.97 3.33 26.81-15.1 27.58-19.77 8.28-50.03 12.06-101.21 11.27-152.11-.88-55.8-47.94-101.88-104.91-102.72zm-110.69-19.78c-10.3-8.34-25.37-6.8-33.76 3.48-25.62 31.5-39.39 71.28-38.75 112 .59 37.58-2.47 75.27-9.11 112.05-2.34 13.05 6.31 25.53 19.36 27.89 20.11 3.5 27.07-14.81 27.89-19.36 7.19-39.84 10.5-80.66 9.86-121.33-.47-29.88 9.2-57.88 28-80.97 8.35-10.28 6.79-25.39-3.49-33.76zm109.47-62.33c-15.41-.41-30.87 1.44-45.78 4.97-12.89 3.06-20.87 15.98-17.83 28.89 3.06 12.89 16 20.83 28.89 17.83 11.05-2.61 22.47-3.77 34-3.69 75.43 1.13 137.73 61.5 138.88 134.58.59 37.88-1.28 76.11-5.58 113.63-1.5 13.17 7.95 25.08 21.11 26.58 16.72 1.95 25.51-11.88 26.58-21.11a929.06 929.06 0 0 0 5.89-119.85c-1.56-98.75-85.07-180.33-186.16-181.83zm252.07 121.45c-2.86-12.92-15.51-21.2-28.61-18.27-12.94 2.86-21.12 15.66-18.26 28.61 4.71 21.41 4.91 37.41 4.7 61.6-.11 13.27 10.55 24.09 23.8 24.2h.2c13.17 0 23.89-10.61 24-23.8.18-22.18.4-44.11-5.83-72.34zm-40.12-90.72C417.29 43.46 337.6 1.29 252.81.02 183.02-.82 118.47 24.91 70.46 72.94 24.09 119.37-.9 181.04.14 246.65l-.12 21.47c-.39 13.25 10.03 24.31 23.28 24.69.23.02.48.02.72.02 12.92 0 23.59-10.3 23.97-23.3l.16-23.64c-.83-52.5 19.16-101.86 56.28-139 38.76-38.8 91.34-59.67 147.68-58.86 69.45 1.03 134.73 35.56 174.62 92.39 7.61 10.86 22.56 13.45 33.42 5.86 10.84-7.62 13.46-22.59 5.84-33.43z" transform="translate(-256 -256)" class=""></path></g></g></mask></defs><rect fill="currentColor" clip-path="url(#clip-rZlolpo0ZmCK)" mask="url(#mask-7IfAyCrJnlDP)" x="0" y="0" width="100%" height="100%" class=""></rect></svg></span>
-                        <span class="tweetEntry-username">
-                            <b>${smartTruncate(msg.pubkey, 20)}</b>
-                        </span>
-                        <span class="tweetEntry-timestamp">- ${moment(msg.timestamp).format('L h:m:s a')}</span>
-                        <div class="tweetEntry-text-container">
-                            ${result.message}
+        if (msg.signed !== undefined && msg.pubkey !== undefined) {
+            verify(msg.signed, msg.pubkey).then(result => {
+                if (result.message) {
+                    div.innerHTML = `
+                    <div class="row">
+                        <div id="d-public.${msg.timestamp}~${msg.pubkey}" class="tweetEntry-content">
+                            <span class="tweetEntry-avatar"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="fingerprint" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-fingerprint fa-w-14 fa-9x"><defs class=""><clipPath id="clip-rZlolpo0ZmCK" class=""><path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z" class=""></path></clipPath><mask x="0" y="0" width="100%" height="100%" id="mask-7IfAyCrJnlDP" maskUnits="userSpaceOnUse" maskContentUnits="userSpaceOnUse" class=""><rect x="0" y="0" width="100%" height="100%" fill="white" class=""></rect><g transform="translate(224 256)" class=""><g transform="translate(0, 0)  scale(0.5625, 0.5625)  rotate(0 0 0)" class=""><path fill="black" d="M256.12 245.96c-13.25 0-24 10.74-24 24 1.14 72.25-8.14 141.9-27.7 211.55-2.73 9.72 2.15 30.49 23.12 30.49 10.48 0 20.11-6.92 23.09-17.52 13.53-47.91 31.04-125.41 29.48-224.52.01-13.25-10.73-24-23.99-24zm-.86-81.73C194 164.16 151.25 211.3 152.1 265.32c.75 47.94-3.75 95.91-13.37 142.55-2.69 12.98 5.67 25.69 18.64 28.36 13.05 2.67 25.67-5.66 28.36-18.64 10.34-50.09 15.17-101.58 14.37-153.02-.41-25.95 19.92-52.49 54.45-52.34 31.31.47 57.15 25.34 57.62 55.47.77 48.05-2.81 96.33-10.61 143.55-2.17 13.06 6.69 25.42 19.76 27.58 19.97 3.33 26.81-15.1 27.58-19.77 8.28-50.03 12.06-101.21 11.27-152.11-.88-55.8-47.94-101.88-104.91-102.72zm-110.69-19.78c-10.3-8.34-25.37-6.8-33.76 3.48-25.62 31.5-39.39 71.28-38.75 112 .59 37.58-2.47 75.27-9.11 112.05-2.34 13.05 6.31 25.53 19.36 27.89 20.11 3.5 27.07-14.81 27.89-19.36 7.19-39.84 10.5-80.66 9.86-121.33-.47-29.88 9.2-57.88 28-80.97 8.35-10.28 6.79-25.39-3.49-33.76zm109.47-62.33c-15.41-.41-30.87 1.44-45.78 4.97-12.89 3.06-20.87 15.98-17.83 28.89 3.06 12.89 16 20.83 28.89 17.83 11.05-2.61 22.47-3.77 34-3.69 75.43 1.13 137.73 61.5 138.88 134.58.59 37.88-1.28 76.11-5.58 113.63-1.5 13.17 7.95 25.08 21.11 26.58 16.72 1.95 25.51-11.88 26.58-21.11a929.06 929.06 0 0 0 5.89-119.85c-1.56-98.75-85.07-180.33-186.16-181.83zm252.07 121.45c-2.86-12.92-15.51-21.2-28.61-18.27-12.94 2.86-21.12 15.66-18.26 28.61 4.71 21.41 4.91 37.41 4.7 61.6-.11 13.27 10.55 24.09 23.8 24.2h.2c13.17 0 23.89-10.61 24-23.8.18-22.18.4-44.11-5.83-72.34zm-40.12-90.72C417.29 43.46 337.6 1.29 252.81.02 183.02-.82 118.47 24.91 70.46 72.94 24.09 119.37-.9 181.04.14 246.65l-.12 21.47c-.39 13.25 10.03 24.31 23.28 24.69.23.02.48.02.72.02 12.92 0 23.59-10.3 23.97-23.3l.16-23.64c-.83-52.5 19.16-101.86 56.28-139 38.76-38.8 91.34-59.67 147.68-58.86 69.45 1.03 134.73 35.56 174.62 92.39 7.61 10.86 22.56 13.45 33.42 5.86 10.84-7.62 13.46-22.59 5.84-33.43z" transform="translate(-256 -256)" class=""></path></g></g></mask></defs><rect fill="currentColor" clip-path="url(#clip-rZlolpo0ZmCK)" mask="url(#mask-7IfAyCrJnlDP)" x="0" y="0" width="100%" height="100%" class=""></rect></svg></span>
+                            <span class="tweetEntry-username">
+                                <b>${smartTruncate(msg.pubkey, 20)}</b>
+                            </span>
+                            <span class="tweetEntry-timestamp">- ${moment(msg.timestamp).format('L h:m:s a')}</span>
+                            <div class="tweetEntry-text-container">
+                                ${result.message}
+                            </div>
                         </div>
+                        <form id="f.public.${msg.timestamp}~${msg.pubkey}">
+                            <div class="optionalMedia" style="display:none;">
+                                <input id="public.${msg.timestamp}~${msg.pubkey}" type="text" value="superman">
+                                <img class="optionalMedia-img" src="https://i.imgur.com/kOhhPAk.jpg">
+                            </div>
+                            <div class="tweetEntry-action-list" style="line-height:32px;color: #b1bbc3;">
+                                <i id="${msg.hash}" onclick="showreply('${msg.hash}')" class="fa fa-reply" style="width: 80px; cursor: pointer;"></i>
+                            </div>
+                        </form>
                     </div>
-                    <form id="f.public.${msg.timestamp}~${msg.pubkey}">
-                        <div class="optionalMedia" style="display:none;">
-                            <input id="public.${msg.timestamp}~${msg.pubkey}" type="text" value="superman">
-                            <img class="optionalMedia-img" src="https://i.imgur.com/kOhhPAk.jpg">
-                        </div>
-                        <div class="tweetEntry-action-list" style="line-height:32px;color: #b1bbc3;">
-                            <i id="${msg.hash}" onclick="showreply('${msg.hash}')" class="fa fa-reply" style="width: 80px; cursor: pointer;"></i>
-                        </div>
-                    </form>
-                </div>
-                `
-        })
+                    `
+                }
+            })
+        }
         target.parentNode.insertBefore(div, target.nextSibling);
     });
 
