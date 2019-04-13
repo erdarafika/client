ec74ddcfd21f2029330ca31cfbda1a2bab8177ca
unknown <muhanwar@digiro.in>
2019-04-13 18:49:18 +0700

    Add audio comment.

--- public/js/main.js
+++ public/js/main.js
@@ -726,42 +726,10 @@ const comAudio = async (id) => {
     const recorder = await commentAudio(id);
     const actionButton = document.getElementById('actAudio');
     actionButton.disabled = true;
-    // recorder.start();
-    new Promise(async resolve => {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        const mediaRecorder = new MediaRecorder(stream);
-        const audioChunks = [];
-    
-        mediaRecorder.addEventListener("dataavailable", event => {
-          audioChunks.push(event.data);
-        });
-    
-        // const start = () => mediaRecorder.start();
-        mediaRecorder.start();
-    
-        new Promise(resolve => {
-            mediaRecorder.addEventListener("stop", () => {
-              const audioBlob = new Blob(audioChunks);
-              const audioUrl = URL.createObjectURL(audioBlob);
-              const audio = new Audio(audioUrl);
-              const play = () => audio.play();
-              blob2abuff(audioBlob).then(data => {
-                const uint8View = new Uint8Array(data);
-                const hex = buf2hex(uint8View)
-                const pair = localStorage.getItem('pair')
-                const key = JSON.parse(pair)
-                post(id, 'replies', {message: hex, type: "audio"}, key).then(res => {
-                    const msg = JSON.parse(res)
-                    const sig = "SEA"+JSON.stringify({m: {message: msg.message, type: "audio"}, s: msg.sig})
-                })
-              })
-              resolve({ audioBlob, audioUrl, play });
-            });
-        });
-    });
+    recorder.start();
     // await sleep(9000);
-    // const audio = await recorder.stop();
-    // audio.play();
+    const audio = await recorder.stop();
+    audio.play();
     // await sleep(9000);
     // actionButton.disabled = false;
 }
