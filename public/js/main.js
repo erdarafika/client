f41530e9e7ea66bb448c4cddc5829e5f5acf1b1b
unknown <muhanwar@digiro.in>
2019-04-13 11:27:29 +0700

    Add audio comment.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 /*** 
     Holla universe!
 ***/
-const presenter = new ScarletsMediaPresenter();
-const streamer = new ScarletsAudioStreamer();
+const presenter = new ScarletsMediaPresenter(.);
+const streamer = new ScarletsAudioStreamer(.);
 
 Gun.on('opt', function (ctx) {
   if (ctx.once) {
