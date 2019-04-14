4f978205f8baa656728763644ca6281d63b1fce4
unknown <muhanwar@digiro.in>
2019-04-14 19:26:52 +0700

    Turn off all local storage.

--- public/js/main.js
+++ public/js/main.js
@@ -832,7 +832,7 @@ const commentAudio = (id) =>
     // audio.play();
     // await sleep(42000);
     // actionButton.disabled = false;
-}
+// }
 
 const comAudio = async (id) => {
     // navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(stream => {
