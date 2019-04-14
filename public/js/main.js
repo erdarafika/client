302ba46f6c2e79300a09f15314feea404fd62ee8
unknown <muhanwar@digiro.in>
2019-04-14 21:15:06 +0700

    Turn off all local storage.

--- public/js/main.js
+++ public/js/main.js
@@ -21,6 +21,77 @@ var mediaSource = new MediaSource(); // mediaSource.readyState === 'closed'
 // video.src = URL.createObjectURL(mediaSource);
 // Wait for media source to be open
 
+mediaSource.addEventListener('sourceopen', handleSourceOpen.bind(mediaSource));
+function handleSourceOpen() {
+  var mediaSource = this; // mediaSource.readyState === 'open'
+  var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+  /*** 
+    "Parse" manifest -> segments
+  
+        <Representation
+          id="video=1660000"
+          bandwidth="1660000"
+          width="1280"
+          height="720"
+          sar="1:1"
+          codecs="avc1.4D401F"
+          scanType="progressive">
+          <SegmentTemplate
+            timescale="12800"
+            initialization="vinn-$RepresentationID$.dash"
+            media="vinn-$RepresentationID$-$Time$.dash">
+            <SegmentTimeline>
+              <S t="0" d="25600" r="51" />
+              <S d="20992" />
+            </SegmentTimeline>
+          </SegmentTemplate>
+        </Representation>
+  ***/
+ 
+  mediaSource.duration = 6; // (51200 + 25600) / 12800
+  // Fetch init segment (contains mp4 header)
+  fetchSegmentAndAppend("https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000.dash", sourceBuffer, function() {
+    function iter() {
+      // Pop segment from queue
+      var url = queue.shift();
+      if (url === undefined) {
+        return;
+      }
+      // Download segment and append to source buffer
+      fetchSegmentAndAppend(url, sourceBuffer, function(err) {
+        if (err) {
+          console.error(err);
+        } else {
+          setTimeout(iter, 200);
+        }
+      });
+    }
+    iter();
+    video.play();
+  });
+}
+
+function fetchSegmentAndAppend(segmentUrl, sourceBuffer, callback) {
+    fetchArrayBuffer(segmentUrl, function(buf) {
+      sourceBuffer.addEventListener('updateend', function(ev) {
+        callback();
+      });
+      sourceBuffer.addEventListener('error', function(ev) {
+        callback(ev);
+      });
+      sourceBuffer.appendBuffer(buf);
+    });
+  }
+  function fetchArrayBuffer(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function() {
+      callback(xhr.response);
+    };
+    xhr.send();
+  }
+
 const toHexString = function toHexString(byteArray) {
   return Array.prototype.map.call(byteArray, function(byte) {
     return ('0' + (byte & 0xFF).toString(16)).slice(-2);
@@ -839,77 +910,6 @@ class App {
         } else {
             notsigned()
         }
-
-mediaSource.addEventListener('sourceopen', handleSourceOpen.bind(mediaSource));
-function handleSourceOpen() {
-  var mediaSource = this; // mediaSource.readyState === 'open'
-  var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-  /*** 
-    "Parse" manifest -> segments
-  
-        <Representation
-          id="video=1660000"
-          bandwidth="1660000"
-          width="1280"
-          height="720"
-          sar="1:1"
-          codecs="avc1.4D401F"
-          scanType="progressive">
-          <SegmentTemplate
-            timescale="12800"
-            initialization="vinn-$RepresentationID$.dash"
-            media="vinn-$RepresentationID$-$Time$.dash">
-            <SegmentTimeline>
-              <S t="0" d="25600" r="51" />
-              <S d="20992" />
-            </SegmentTimeline>
-          </SegmentTemplate>
-        </Representation>
-  ***/
- 
-  mediaSource.duration = 6; // (51200 + 25600) / 12800
-  // Fetch init segment (contains mp4 header)
-  fetchSegmentAndAppend("https://testcontent.eyevinn.technology/mse-tutorial/vinn-video=1660000.dash", sourceBuffer, function() {
-    function iter() {
-      // Pop segment from queue
-      var url = queue.shift();
-      if (url === undefined) {
-        return;
-      }
-      // Download segment and append to source buffer
-      fetchSegmentAndAppend(url, sourceBuffer, function(err) {
-        if (err) {
-          console.error(err);
-        } else {
-          setTimeout(iter, 200);
-        }
-      });
-    }
-    iter();
-    video.play();
-  });
-}
-
-function fetchSegmentAndAppend(segmentUrl, sourceBuffer, callback) {
-    fetchArrayBuffer(segmentUrl, function(buf) {
-      sourceBuffer.addEventListener('updateend', function(ev) {
-        callback();
-      });
-      sourceBuffer.addEventListener('error', function(ev) {
-        callback(ev);
-      });
-      sourceBuffer.appendBuffer(buf);
-    });
-  }
-  function fetchArrayBuffer(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url);
-    xhr.responseType = 'arraybuffer';
-    xhr.onload = function() {
-      callback(xhr.response);
-    };
-    xhr.send();
-  }
     }
 }
 
