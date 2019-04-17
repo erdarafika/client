260442b908930bac53790a8f50da43e9cbe87e85
unknown <muhanwar@digiro.in>
2019-04-17 10:37:18 +0700

    Try video streaming.

--- public/js/main.js
+++ public/js/main.js
@@ -483,11 +483,11 @@ const sig = function signed() {
         </div>
         `
     main.appendChild(p)
-    // const videoTag = document.getElementById("my-video");
-    // const myMediaSource = new MediaSource();
-    // const url = URL.createObjectURL(myMediaSource);
-    // videoTag.src = url;
-    // const videoSourceBuffer = myMediaSource.addSourceBuffer('video/webm; codecs=vp9,opus');
+    const videoTag = document.getElementById("my-video");
+    const myMediaSource = new MediaSource();
+    const url = URL.createObjectURL(myMediaSource);
+    videoTag.src = url;
+    const videoSourceBuffer = myMediaSource.addSourceBuffer('video/webm; codecs=vp9,opus');
     gun.get('posts').map().on(function(data) {
         let target = document.getElementById('main')
         let div = document.createElement('div')
