9e087ec02d31dd01eed8a2abb0a61ddc1261ae0e
unknown <muhanwar@digiro.in>
2019-04-17 20:24:33 +0700

    Try video streaming.

--- public/js/main.js
+++ public/js/main.js
@@ -510,12 +510,12 @@ const sig = function signed() {
                             // console.log(queue.length) 
                             // now just call queue.push(videoSourceBuffer) instead
                             // videoSourceBuffer.appendBuffer(hex2byte(result.message));
-                            if (videoSourceBuffer.updating || myMediaSource.readyState != "open" || queue.length > 0) {
-                                console.log(queue.length)
-                                videoSourceBuffer.appendBuffer(queue.shift());
-                            } else {
+                            // if (videoSourceBuffer.updating || myMediaSource.readyState != "open" || queue.length > 0) {
+                            //     console.log(queue.length)
+                            //     videoSourceBuffer.appendBuffer(queue.shift());
+                            // } else {
                                 
-                            }
+                            // }
                             
                         //     const blob = new Blob([hex2byte(result.message)], {
                         //         type: 'video/webm; codecs="opus,vp9"'
